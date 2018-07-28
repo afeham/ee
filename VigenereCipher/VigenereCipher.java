b89f21584a01f55a2d7c7f4c63be9783ecf20ecc
sakun <7917503+sakunc@users.noreply.github.com>
2018-07-28 11:47:51 -0400

    Create VigenereCipher.java

---
+++ VigenereCipher/VigenereCipher.java
@@ -0,0 +1,21 @@
+public class VigenereCipher {
+    public static void main(String[] args){
+        String text = "APPLES";
+        String key = "DOG";
+        String encrypted = encrypt(text, key);
+        System.out.println("Your message: " + text);
+        System.out.println("Encrypted Text: " + encrypted);
+    }
+    public static String encrypt(String s, String key){
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < s.length(); i ++){
+            //add shift
+            char encrypted = s.charAt(i) + getShift(key, i) > 90 ? (char)((s.charAt(i) + getShift(key, i)) - 26) : (char)(s.charAt(i) + getShift(key, i));
+            builder.append(encrypted);
+        }
+        return builder.toString();
+    }
+    private static int getShift(String key, int i) {
+        return ((int)key.charAt(i % key.length())) - 65;
+    }
+}
